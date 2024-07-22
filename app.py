22f82783d173191c26fa7b5092f515d4e418c33b
Eli Keli <muthokaeli@gmail.com>
2024-07-22 23:05:57 +0300

    Unique URLs / Redirection Behaviour

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-# URL Building
+# Unique URLs / Redirection Behavior
 
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    about_url = url_for("about")
-    user_url = url_for("show_user_profile", username="Eli")
-    return f'Welcome to the Home Page! <br> About: {about_url} <br> User: {user_url}'
+    return 'Welcome to the Home Page!'
 
-@app.route("/about")
+@app.route("/about/")
 def about():
     return "This is the about page."
+
+@app.route("/contact")
+def contact():
+    return "This is the contact page."
+
+# Basic Redirection
+@app.route('/login')
+def login():
+    return 'This is the Login Page.'
+
+@app.route('/redirect-to-login')
+def redirect_to_login():
+    return redirect(url_for('login'))
+
+# Conditional Redirection
+@app.route("/profile")
+def profile():
+    logged_in = request.args.get("logged_in", 'false').lower() == "true"
+    if not logged_in:
+        return redirect(url_for('login'))
+    return 'This is the Profile Page.'
+
+# HTTP Status Codes
+@app.route('/permanent-redirect')
+def permanent_redirect():
+    return redirect(url_for('home'), code=301)
 
 @app.route("/user/<username>")
 def show_user_profile(username):
