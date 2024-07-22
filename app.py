03a5596af1f1213f11a35e8f83b43dc467b3b13c
Eli Keli <muthokaeli@gmail.com>
2024-07-22 22:29:12 +0300

    Routing and Variable rules

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-# HTML Escaping
+# Routing and Variable rules
 
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    name = "<script>alert('This is an XSS attack!')</script>"
-    return render_template("index.html", name=name)
+def home():
+    return "Welcome to the home page!"
+
+@app.route("/about")
+def about():
+    return "This is the about page."
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return f"User: {username}"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post ID: {post_id}'
 
 
 if __name__ == "__main__":
