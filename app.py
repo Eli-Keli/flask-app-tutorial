a782123a50386659bb8390ba29572bad2ac8ccef
Eli Keli <muthokaeli@gmail.com>
2024-07-23 21:53:12 +0300

    File Uploads

--- app.py
+++ app.py
@@ -1,34 +1,44 @@
-# Static Files
 
-from flask import Flask, send_from_directory, render_template
+#* File Uploads
+
+from flask import Flask, redirect, url_for, request,render_template
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return '''
-    <html>
-        <head>
-            <link rel="stylesheet" type="text/css" href="/static/css/style.css">
-        </head>
-        <body>
-            <h1>Welcome to the Home Page!</h1>
-            <button>Click</button>
-            <img src="/static/images/logo.png" alt="Logo">
-            <script src="/static/js/script.js"></script>
-        </body>
-    </html>
-    '''
+# Set the folder
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# Using Static Files in Templates
-@app.route("/index")
+# Set file size limit
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('upload.html')
 
-# Custom Static File Route
-@app.route('/custom_static/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('static/css', filename)
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+    elif file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return f'File successfully uploaded: {filename}'
+    else:
+        return 'File type not allowed'
 
 if __name__ == "__main__":
     app.run(debug=True)
