e30ca7ee817cb50376df8f2a69727e0cc1cb83e5
Eli Keli <muthokaeli@gmail.com>
2024-07-24 22:55:08 +0300

    About Responses

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 
-#* File Uploads
-from flask import Flask, redirect, render_template
+#* About Responses
+from flask import Flask, make_response, jsonify, send_file
 
 
 app = Flask(__name__)
 
+# Basic Response
 @app.route("/")
 def index():
-    return "<h1>Welcome to the Home page.</h1>"
+    return "Welcome to the Home page."
 
-@app.route('/cause-404')
-def cause_404():
-    # This will cause a 404 error because the route does not exist
-    return redirect('/non_existent_page')
+# Custom Responses
+@app.route("/custom")
+def custom():
+    response = make_response('This is a custom response.')
+    response.headers['Content-Type'] = 'text/plain'
+    response.status_code = 200
+    return response
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"),404
+# JSON Response
+@app.route("/json")
+def json_example():
+    return jsonify(message="This is a JSON response")
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html"),500
+# Setting Headers
+@app.route("/custom-header")
+def custom_headers():
+    response = make_response('Hello, with custom headers!')
+    response.headers['X-Custom-Header'] = 'CustomHeaderValue'
+    return response
 
+# Setting status codes
+@app.route('/not-found')
+def not_found():
+    response = make_response('This resource was not found', 404)
+    return response
+
+# File Download
+@app.route('/download')
+def download_file():
+    path = './downloads/download.txt'
+    return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
